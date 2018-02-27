4b1dc000e2af0b80814604612575c409e5835f39
HuynhMinhTin <tin19962014@gmail.com>
2018-02-27 23:13:59 +0700

    Commit spring mvc setup

---
+++ minisharingfiles/src/main/java/com/dxc/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.dxc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+	@ResponseBody
+	public String Default () {
+		return "home";
+	}
+	
+}
